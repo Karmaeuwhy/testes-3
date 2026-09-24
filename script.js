98c2de14a65e12a99137695a5903983c5d6d785f
Karmaeuwhy <srgame228@gmail.com>
2026-09-24 13:58:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const o=new IntersectionObserver(es=>es.forEach(e=>{if(e.isIntersecting){e.target.classList.add("visible");o.unobserve(e.target)}}),{threshold:.12});document.querySelectorAll(".reveal").forEach(x=>o.observe(x));
